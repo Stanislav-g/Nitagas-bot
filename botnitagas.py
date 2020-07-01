673cf8e382b6559b36c58feff376e3ebe542b9b5
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-07-01 15:03:49 +0200

    Update botnitagas.py

--- botnitagas.py
+++ botnitagas.py
@@ -1542,9 +1542,9 @@ async def seagoogletwo(ctx, *, question, question1, question2, question3, questi
 #coinflip
 @client.command()
 async def e(ctx ):
-    a = random.choice(['орел','решка','орел','решка','орел'])
-    r = random.choice(['орел','решка','орел','решка','орел'])
-    url = 'https://drive.google.com/drive/folders/1' + str(a).replace(' ', '+') + str(r).replace(' ', '+') + '?usp=sharing'
+    a = random.choice(['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M','0','1','2','3','4','5','6','7','8','9','_','-')
+    
+    url = 'https://drive.google.com/drive/folders/1' + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + str(a).replace(' ', '+') + '?usp=sharing'
     await ctx.send( url )  
     
     
