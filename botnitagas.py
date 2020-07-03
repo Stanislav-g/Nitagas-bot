40591c6842cc04f001ed4ce2b8b702b13ac88201
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-07-03 15:27:55 +0200

    Update botnitagas.py

--- botnitagas.py
+++ botnitagas.py
@@ -27,15 +27,6 @@ bad_words = ['suka','brandon','дурак','дебил','дибил','tonto','br
 num = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30']
 link = ['https://youtu.be/XVMHRAUI-h0','https://youtu.be/S5WkBjiUQCs']
 
-POST_ID = '728601330406391829'
-
-ROLES = [🏮: 705362629799051325, '💽': 705420927206424596,]
-
-EXCROLES = ()
-
-MAX_ROLES_PER_USER = 2
-
-
 
 @client.event
 
@@ -790,6 +781,24 @@ async def on_raw_reaction_add(payload):
         if str(payload.emoji) == '1️⃣': # Emoji для реакций
             role = guild.get_role(707872645856755752) # ID Ролей для выдачи
         elif str(payload.emoji) == '2️⃣':
+            role = guild.get_role(707912296328069130)
+        
+        if role:
+            member = guild.get_member(payload.user_id)
+            if member:
+                await member.add_roles(role)
+                
+                
+#reactions
+@client.event
+async def on_raw_reaction_add(payload):
+    if payload.message_id == 728601330406391829: # ID Сообщения
+        guild = client.get_guild(payload.guild_id)
+        role = None
+
+        if str(payload.emoji) == '🏮': # Emoji для реакций
+            role = guild.get_role(707872645856755752) # ID Ролей для выдачи
+        elif str(payload.emoji) == '💽':
             role = guild.get_role(707912296328069130)
         
         if role:
@@ -1770,45 +1779,7 @@ async def on_raw_reaction_add(payload):
                 await member.add_roles(role)
     
     
-async def on_raw_reaction_add(self, payload):
-        if payload.message_id == POST_ID:
-            channel = self.get_channel(payload.channel_id) # получаем объект канала
-            message = await channel.fetch_message(payload.message_id) # получаем объект сообщения
-            member = utils.get(message.guild.members, id=payload.user_id) # получаем объект пользователя который поставил реакцию
- 
-            try:
-                emoji = str(payload.emoji) # эмоджик который выбрал юзер
-                role = utils.get(message.guild.roles, id=ROLES[emoji]) # объект выбранной роли (если есть)
-           
-                if(len([i for i in member.roles if i.id not in EXCROLES]) <= MAX_ROLES_PER_USER):
-                    await member.add_roles(role)
-                    print('[SUCCESS] User {0.display_name} has been granted with role {1.name}'.format(member, role))
-                else:
-                    await message.remove_reaction(payload.emoji, member)
-                    print('[ERROR] Too many roles for user {0.display_name}'.format(member))
-           
-            except KeyError as e:
-                print('[ERROR] KeyError, no role found for ' + emoji)
-            except Exception as e:
-                print(repr(e))
- 
-    async def on_raw_reaction_remove(self, payload):
-        channel = self.get_channel(payload.channel_id) # получаем объект канала
-        message = await channel.fetch_message(payload.message_id) # получаем объект сообщения
-        member = utils.get(message.guild.members, id=payload.user_id) # получаем объект пользователя который поставил реакцию
- 
-        try:
-            emoji = str(payload.emoji) # эмоджик который выбрал юзер
-            role = utils.get(message.guild.roles, id=ROLES[emoji]) # объект выбранной роли (если есть)
- 
-            await member.remove_roles(role)
-            print('[SUCCESS] Role {1.name} has been remove for user {0.display_name}'.format(member, role))
- 
-        except KeyError as e:
-            print('[ERROR] KeyError, no role found for ' + emoji)
-        except Exception as e:
-            print(repr(e))
-     
+
     
         
 token= os.environ.get('BOT_TOKEN')
