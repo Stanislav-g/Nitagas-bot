ccf94bdfd7a9fd434a249ac0e3493d3b1bc9928f
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-07-04 14:26:32 +0200

    Update botnitagas.py

--- botnitagas.py
+++ botnitagas.py
@@ -1789,14 +1789,14 @@ async def dis(ctx ):
         t = random.choice(['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M','0','1','2','3','4','5','6','7','8','9'])
              
         url = 'https://discord.gg/' + str(a).replace(' ', '+') + str(q).replace(' ', '+') + str(w).replace(' ', '+') + str(e).replace(' ', '+') + str(r).replace(' ', '+') + str(t).replace(' ', '+') 
-        await ctx.send(f's')
+        
         r = requests.get(url)
         if r.status_code == 404:
             continue 
             
         else:
             await ctx.send( url )
-            await ctx.author.send( url )
+            
 
 
 
