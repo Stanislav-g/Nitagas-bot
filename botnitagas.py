50bc5105be6b7f2cc246b6084dbc3459613a7ccb
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-07-18 21:03:44 +0200

    Update botnitagas.py

--- botnitagas.py
+++ botnitagas.py
@@ -1922,15 +1922,16 @@ async def infofor(ctx , member:discord.Member):
     if not Member:
         Member = ctx.author
     roles = (role for role in Member.roles )
-    emb = discord.Embed(title='Информация о пользователе.'.format(Member.name), description=
-                        f"Участник зашёл на сервер: {Member.joined_at.strftime('%b %#d, %Y')}\n\n "
-                        f"Имя: {Member.name}\n\n"
-                        f"Никнейм: {Member.nick}\n\n"
-                        f"Статус: {Member.status}\n\n"
-                        f"ID: {Member.id}\n\n"
-                        f"Высшая роль: {Member.top_role}\n\n"
-                        f"Аккаунт создан: {Member.created_at.strftime('%b %#d, %Y')}", 
-                        color = 0xff0000, timestamp=ctx.message.created_at)
+    emb = discord.Embed(title='Информация о пользователе.'.format(Member.name), description=f"Участник зашёл на сервер: {Member.joined_at.strftime('%b %#d, %Y')}\n\n "
+                                                                                      f"Имя: {Member.name}\n\n"
+                                                                                      f"Никнейм: {Member.nick}\n\n"
+                                                                                      f"Статус: {Member.status}\n\n"
+                                                                                      f"ID: {Member.id}\n\n"
+                                                                                      f"Высшая роль: {Member.top_role}\n\n"
+                                                                                      f"Аккаунт создан: {Member.created_at.strftime('%b %#d, %Y')}", 
+                                                                                      color=0xff0000, timestamp=ctx.message.created_at)
+
+   
 
     emb.set_thumbnail(url= Member.avatar_url)
     emb.set_footer(icon_url= Member.avatar_url)
