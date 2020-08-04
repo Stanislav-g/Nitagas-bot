c8505f28f60b15f24a3bad474bf6084dd5f3c9fb
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-08-04 11:36:56 +0200

    Add files via upload

---
+++ lib.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import datetime
+from discord.utils import get
+import asyncio
+from time import sleep
+from colorsys import hls_to_rgb
+import youtube_dl
+import os
+import requests
+import random
+from random import randint, choice, choices
+import io
+import sqlite3
+import random as r
+import requests
+import io
+import typing
