e6e9236a26f8c6dd176acbc2f458efb200a1fe24
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-08-04 11:37:33 +0200

    Update botnitagas.py

--- botnitagas.py
+++ botnitagas.py
@@ -1,21 +1,5 @@
-import discord
-from discord.ext import commands
-import datetime
-from discord.utils import get
-import asyncio
-from time import sleep
-from colorsys import hls_to_rgb
-import youtube_dl
-import os
-import requests
-import random
-from random import randint, choice, choices
-import io
-import sqlite3
-import random as r
-import requests
-import io
-import typing
+import lib
+
 
 client = commands.Bot( command_prefix = '-')
 client.remove_command('help')
