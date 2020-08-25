5712cfd9591efdb7c0edfa2dd57dbbf5126ba01f
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-08-25 12:42:18 +0300

    Update botnitagas.py

--- botnitagas.py
+++ botnitagas.py
@@ -1241,7 +1241,7 @@ async def on_guild_update(before, after):
         embed = discord.Embed(color=discord.Color.green(), timestamp=datetime.datetime.now(datetime.timezone.utc), description=f'**Guild name was changed**')
         embed.add_field(name='Before', value=before.name, inline=False)
         embed.add_field(name='After', value=after.name, inline=False)
-	embed.set_author(name=after.name, icon_url=str(after.icon_url))
+        embed.set_author(name=after.name, icon_url=str(after.icon_url))
 	embed.set_footer(text=f"Guild ID: {after.id}")
 	await channel.send(embed=embed)
 				
