e522d3c1cec86518b7028cb620acea7fdf037a2e
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-08-25 16:18:35 +0300

    Update botnitagas.py

--- botnitagas.py
+++ botnitagas.py
@@ -1313,7 +1313,6 @@ async def on_member_edit(before, after):
     channel = client.get_channel( 747764481559494686 )
     if nick.after != nick.before:
         embed = discord.Embed(color=after.author.color, timestamp=after.created_at, description=f'{after.author.mention} **changed his nick** {after.channel.mention}')
-    	embed.set_author(name=after.author, icon_url=str(after.author.avatar_url_as(static_format='png', size=2048)))
     	bcontent = before.system_content[:300] + (before.system_content[300:] and '...')
    	acontent = after.system_content[:300] + (after.system_content[300:] and '...')
 	embed.add_field(name='Before', value=bcontent, inline=False)
