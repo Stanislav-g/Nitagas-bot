af920e650c24246a673613a6f45598db7e3ea504
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-08-25 15:38:05 +0300

    Update botnitagas.py

--- botnitagas.py
+++ botnitagas.py
@@ -1307,6 +1307,67 @@ async def on_message_edit(before, after):
     embed.add_field(name='After', value=acontent, inline=False)
     embed.set_footer(text=f"Author ID: {after.author.id} | Message ID: {after.id} | Channel ID: {after.channel.id}")
     await channel.send(embed=embed)
-		
+
+@client.event
+async def on_member_remove(member):
+    if not [g for g in self.bot.guilds if g.get_member(member.id)]:
+        self.bot.configs.pop(member.id, None)
+        conf = self.bot.get_config(member.guild)
+        if conf.get('greet.leavemsg'):
+            leavechan = conf.get('greet.leavechannel')
+            leavemsg = conf.get('greet.leavemsg')
+            if leavechan and leavemsg:
+                vars = {
+                    '{user.mention}': member.mention,
+                    '{user}': str(member),
+                    '{user.name}': member.name,
+                    '{user.discrim}': member.discriminator,
+                    '{server}': str(member.guild),
+                    '{guild}': str(member.guild),
+                    '{count}': str(member.guild.member_count),
+                    '@everyone': u'@\u200beveryone',  # Nobody needs @everyone in a join/leave message
+                    '@here': u'@\u200bhere'
+                }
+                message = leavemsg
+                for var, value in vars.items():
+                    message = message.replace(var, value)
+                await leavechan.send(message)
+        logch = conf.get('log.moderation')
+        if logch:
+            moderator = None
+            action = None
+            if member.guild.me.guild_permissions.view_audit_log:
+                async for e in member.guild.audit_logs(limit=5):
+                    if e.action in [discord.AuditLogAction.kick, discord.AuditLogAction.ban] and e.target.id == member.id:
+                        moderator = e.user
+                        if moderator == member.guild.me:
+                            moderator = None
+                            break
+                        if e.action == discord.AuditLogAction.kick:
+                            action = 'Kicked'
+                            reason = e.reason
+                        if e.action == discord.AuditLogAction.ban:
+                            action = 'Banned'
+                            reason = e.reason
+                        break
+            embed = discord.Embed(title='Member Left', url='https://i.giphy.com/media/5C0a8IItAWRebylDRX/source.gif', color=discord.Color.red(), timestamp=datetime.datetime.now(datetime.timezone.utc))
+            embed.set_author(name=f'{member}', icon_url=str(member.avatar_url_as(static_format='png', size=2048)))
+            if member.nick:
+                embed.add_field(name='Nickname', value=member.nick, inline=False)
+            roles = [role.mention for role in member.roles if role != member.guild.default_role]
+            if roles:
+                embed.add_field(name='Roles', value=', '.join(roles), inline=False)
+            if moderator and action:
+                embed.add_field(name=f'{action} By', value=f'{moderator} ({moderator.id})', inline=False)
+            if action and reason:
+                embed.add_field(name=f'{action} for', value=reason, inline=False)
+            embed.set_footer(text=f'User ID: {member.id}')
+            try:
+                await logch.send(embed=embed)
+            except Exception:
+                pass
+
+
+
 token= os.environ.get('BOT_TOKEN')
 client.run( token )
