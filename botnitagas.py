55b7cf42df698297906401b6ae8d804786d705a6
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-08-25 12:59:11 +0300

    Update botnitagas.py

--- botnitagas.py
+++ botnitagas.py
@@ -1245,7 +1245,7 @@ async def on_guild_name_update(before, after):
     await channel.send(embed=embed)
 				
 @client.event
-async def on_message_delite( message ):
+async def on_message_delete( message ):
     channel = client.get_channel( 705392639863685170 )
     embed = discord.Embed(color=discord.Color.green(), timestamp=datetime.datetime.now(datetime.timezone.utc), description=f'**A message was delited**\n{message}\n{message.author.name}')
     embed.set_author(name=role.guild.name, icon_url=str(role.guild.icon_url))
