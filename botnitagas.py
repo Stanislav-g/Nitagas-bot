f0c9b0c14f8ef8a57ccd313f9b0dcdf131d72583
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-08-28 10:54:35 +0300

    Update botnitagas.py

--- botnitagas.py
+++ botnitagas.py
@@ -853,11 +853,9 @@ async def tempmute(ctx, amount : int, member: discord.Member = None, reason = No
     channel = client.get_channel(747764481559494686) #Айди канала логов
     mutee_role = discord.utils.get(member.guild.roles, id = 705745998550401054) #Айди роли
     await member.add_roles( mutee_role )
-    embed = discord.Embed(description = f':shield: Пользователю {member.mention} был ограничен доступ к чатам.\n:book: По причине: {reason}\n:clock: На {amount} секунд\n:face_with_monocle: Ограничил доступ {ctx.author.mention}', color=0x0c0c0c)
-    embed.set_footer(text=f"Message ID: {message.id}")
+    embed = discord.Embed(description = f':shield: Пользователю {member.mention} был ограничен доступ к чатам.\n:book: По причине: {reason}\n:clock: На {amount} секунд\n:face_with_monocle: Ограничил доступ {ctx.author.mention}', color=0x0c0c0c)}")
     await ctx.send(embed=embed)  
     embedd = discord.Embed(description = f':shield: Пользователю {member.mention} был ограничен доступ к чатам.\n:book: По причине: {reason}\n:clock: На {amount} секунд\n:face_with_monocle: Ограничил доступ {ctx.author.mention}', color=0x0c0c0c)
-    embedd.set_footer(text=f"Message ID: {message.id}")
     await channel.send(embed=embedd)
     await asyncio.sleep(amount)
     await member.remove_roles( mutee_role )
