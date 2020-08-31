627d6445efa1fab0731fb54d7289ead1889683ac
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-08-31 23:46:30 +0300

    Update botnitagas.py

--- botnitagas.py
+++ botnitagas.py
@@ -1310,6 +1310,7 @@ async def country( ctx ):
 		    allnums = int(num + num2)
 		    num3 = int(between - num2)
 		    if num > num2 and num > num3:
+			await ctx.send(f"test")
 			if num2 > num3:
 			    await ctx.send('1', randomflag, num,'%')
 			    await ctx.send('2', randomflag2, num2,'%')
