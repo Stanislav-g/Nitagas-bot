4e0e3b89859c6d30e486aefc04b3bc140a968247
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-08-31 23:29:59 +0300

    Update botnitagas.py

--- botnitagas.py
+++ botnitagas.py
@@ -1308,8 +1308,7 @@ async def country( ctx ):
 
 	    if summa <= 100:
 	        allnums = num + num2
-		oneh = 100
-		between_second = oneh - allnums
+		between_second = between - num2
 		num3 = between_second
 		if num > num2:
 
