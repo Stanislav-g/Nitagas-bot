03643babac0a1a64bdfd11d98dc8c6873068da6e
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-09-01 08:50:56 +0300

    Update botnitagas.py

--- botnitagas.py
+++ botnitagas.py
@@ -31,8 +31,8 @@ async def country( ctx ):
     randomflag = random.choice(['flag','flag2','flag3'])
     if num < 100:
 	    between = int(100 - num)
-	num2 = random.randint(1,between)
-	summa = (num + num2)		
+	    num2 = random.randint(1,between)
+	    summa = (num + num2)		
         if summa <= 100:			
 	    allnums = int(num + num2)
 	    between_second = (100 - allnums)
