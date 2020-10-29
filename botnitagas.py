95f82763dd3b4c0364e425aaceab5f85c9b04e86
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-10-29 19:20:32 +0100

    Update botnitagas.py

--- botnitagas.py
+++ botnitagas.py
@@ -1459,18 +1459,7 @@ async def on_invite_create(invite: discord.Invite):
    
 
 
-@client.command()
-@commands.has_permissions( kick_members = True )
-async def tempmute(ctx, amount : int, member: discord.Member = None, * ,reason = None):
-    await ctx.channel.purge( limit = 1 )
-    channel = client.get_channel(747764481559494686) #Айди канала логов
-    mutee_role = discord.utils.get(member.guild.roles, id = 705745998550401054) #Айди роли
-    await member.add_roles( mutee_role )
-    embed = discord.Embed(description = f':shield: Пользователю {member.mention} был ограничен доступ к чатам. \n📖 По причине: {reason}\n🕰️ На {amount} секунд\n🧐 Ограничил доступ {ctx.author.mention}', color=0x0c0c0c)
-    embed.set_footer(text=f"ID Пользователя: {ctx.author.id}")
-    await ctx.send(embed=embed)  
-    await asyncio.sleep(amount)
-    await member.remove_roles( mutee_role )
+
 
 @client.command()
 async def tempmute(ctx, amount : int = None, member: discord.Member = None, * ,reason = None):
